bfd12187f0f2339d82bc10fa4f73414208022ec7
Daniel <dsalvatori2008@gmail.com>
2023-01-23 10:51:51 +0000

    done

--- src/containers/StockBox.js
+++ src/containers/StockBox.js
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from "react";
 import StockList from "../components/StockList";
-
 import Data from "../testPageData";
 import StockProbability from "../components/StockProbability";
 
 
+// .map fuck
 const StockBox = () => {
     
     const [stockList, setStockList] = useState([])
     const [stockProp, setStockProp] = useState('')
-    const [stockForDetail, setStockforDetail] = useState('')
+    const [stockForDetail, setStockforDetail] = useState([])
     const [ticker, setTicker] = useState('TSLA')
 
 
-    
+
     let url = 'https://api.stockdata.org/v1/data/quote?symbols=AAPL%2CTSLA%2CMSFT&api_token=f6MJlmpUiiaFPzcSztsLRU6LqpUc27hZCwNHDNMI'
     
     const MoreDetailURLs= {TSLA: 'https://api.stockdata.org/v1/data/intraday?symbols=TSLA&api_token=f6MJlmpUiiaFPzcSztsLRU6LqpUc27hZCwNHDNMI', AAPL: 'https://api.stockdata.org/v1/data/intraday?symbols=AAPL&api_token=f6MJlmpUiiaFPzcSztsLRU6LqpUc27hZCwNHDNMI', MSFT: 'https://api.stockdata.org/v1/data/intraday?symbols=MSFT&api_token=f6MJlmpUiiaFPzcSztsLRU6LqpUc27hZCwNHDNMI'}
@@ -22,11 +22,9 @@ const StockBox = () => {
 
 
     const changeURL = (stockProp, ticker) => {
-        
         setStockProp(stockProp)
         setTicker(ticker)
         loadMoreDetailStock(MoreDetailURLs[ticker])
-        // loadMoreDetailStock(moreDetailURL)
         // url = moreDetailURL
     }
 
